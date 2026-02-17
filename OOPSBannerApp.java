1693e7084db51c1b67e94d1fa22f990636f145a0
pRoy0602 <princeroy1065@gmail.com>
2026-02-18 00:47:10 +0530

    Made UC1

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+*OOPSBannerApp UC1 - OOPS Banner Display Application
+*
+*This class demonstrates  simple Jana application that displays the Object
+*Oriented programming Sysyem OOPS acronym to the console.
+*
+*@auhor Musa
+*@version 1.0
+*/
+public class OOPSBannerApp{
+    public static void main(String[] args){
+	    system.out.println(x:"OOPS");
+		}
+}
