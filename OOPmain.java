677afaee1ce8b8a53ead9fb1870b764665ba447f
pRoy0602 <princeroy1065@gmail.com>
2026-02-18 00:40:07 +0530

    Made main

---
+++ OOPmain.java
@@ -0,0 +1,15 @@
+// This is Main branch
+
+/*
+This is Main branch
+
+@version 1.0
+@author Prince
+
+*/
+
+class OOPmain{
+    public static main(String args[]){
+        system.out.println("This is main branch");
+    }
+}
