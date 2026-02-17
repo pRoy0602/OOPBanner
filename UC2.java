055759569c5c8e2b7fe5c5bb0df6a67bc56bc983
pRoy0602 <princeroy1065@gmail.com>
2026-02-18 00:52:48 +0530

    Made UC2

---
+++ UC2.java
@@ -0,0 +1,16 @@
+/*
+@version 1.0
+@author Musa
+ */
+ 
+ public class Helloworld{
+  public static oid main(string args[]){
+	 System.out.println("   ***   " + "  " + "   ***  " + "  " + " ****** " + "  " + "   *****");
+     System.out.println(" **   **" + "  " + "  **   **" + "  " + " **   **" + "  " + " **");
+     System.out.println("**     **" + "  " + "**     **" + "  " + "**   **" + "  "+ "**");
+     System.out.println("**     **" + "  " + "**     **" + "  " + "****** " + "  " + "  *****");
+     System.out.println("**     **" + "  " +  "**     **" + "  " + "**    " + "  " + "      **");
+     System.out.println(" **   **" + "  " + "  **   **" + "  " + " **    " + "  " + " **   **");
+     System.out.println("   ***   " + "  " + "   ***   " + "  " + "**    " + "  " + "  *****   ");
+  }
+ }
