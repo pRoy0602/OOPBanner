76f3adafa022d216d7b2fbdae38d7cc45246f88b
pRoy0602 <princeroy1065@gmail.com>
2026-04-14 11:54:13 +0530

    made uc1

---
+++ ArrayList.java
@@ -0,0 +1,7 @@
+class ArrayList{
+	public static void main(String[] args){
+		String msg = "This is Main Branch";
+		
+		System.out.println(msg);
+	}
+}
---
+++ UC3.java
@@ -0,0 +1,11 @@
+class UC2OOPS {
+	public static void main(String args[]){
+		System.out.println("******    ******    ****     *******");
+		System.out.println("*    *    *    *    *   *    *");
+		System.out.println("*    *    *    *    *   *    *");
+		System.out.println("*    *    *    *    ****     *******");
+		System.out.println("*    *    *    *    *              *");
+		System.out.println("*    *    *    *    *              *");
+		System.out.println("******    ******    *        *******");
+	}
+}
